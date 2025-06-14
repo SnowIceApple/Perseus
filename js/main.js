796eb372f955a317cde853af1cec8752ae65c2be
SnowIceApple <154928907+SnowIceApple@users.noreply.github.com>
2025-06-15 03:31:25 +0900

    contact 모달 추가

--- js/main.js
+++ js/main.js
@@ -12,6 +12,42 @@ $(document).ready(function(){
         $('.lang_select').toggleClass('active');
     });
 
+    $('.header_contact_btn a').on('click', function(e){
+        e.preventDefault();
+        $('.contact_area').toggleClass('active');
+    })
+
+    $('.modal_open_btn').on('click', function(e){
+        $('.modal_background').toggleClass('active');
+    }); 
+
+    $('.modal_background').on('click', function(e){
+        if($(this).has(e.target).length === 0){
+            $(this).removeClass('active');
+            $('.contact_area').removeClass('active');
+        }
+    });
+
+
+$('.contact_input_box input').each(function(){
+
+    $(this).on('propertychange change keyup paste input', function(){
+        var LastName = $('#LastName').val().length;
+        var FirstName = $('#FirstName').val().length;
+        var Company = $('#Company').val().length;
+        var Email = $('#Email').val().length;
+
+        if(LastName > 0 && FirstName > 0 && Company > 0 && Email > 0){
+            $('.contact_input_box').addClass('confirmed');
+        }
+        else{
+            $('.contact_input_box').removeClass('confirmed');
+        }
+    });
+});
+
+
+
     var pagiArray = ['Efficiency', 'Security', 'Performance'];
 
     const swiper1 = new Swiper('.main_swiper', {
